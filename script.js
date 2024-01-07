123583e4cfcb10f45b7e4bec0a6523d7f22f5df8
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-01-07 09:07:58 +0500

    type script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.dir('h1');
+var data = [
+    {
+        id: 1,
+        title: 'omar',
+        description: 'hello'
+    },
+    {
+        id: 2,
+        title: 'omar',
+        description: 'hello'
+    },
+    {
+        id: 3,
+        title: 'omar',
+        description: 'hello'
+    }
+];
+console.log(data);
--- script.ts
+++ script.ts
@@ -1,5 +1,18 @@
-console.dir('h1');
-let array : boolean = false
-class Car<T>{
-    data:T
-}
+const data = [
+    {
+        id: 1,
+        title: 'omar',
+        description: 'hello'
+    },
+    {
+        id: 2,
+        title: 'omar',
+        description: 'hello'
+    },
+    {
+        id: 3,
+        title: 'omar',
+        description: 'hello'
+    }
+]
+console.log(data);
